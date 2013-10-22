c7f417e8f9c769d77bc4bfadad0a03d1d1c037c8
bryant <blhagadorn@gmail.com>
2013-10-21 20:04:11 -0400

    Friend added

---
+++ QuickEvent/src/main/java/com/redhammerstudios/quickevent/Friend.java
@@ -0,0 +1,64 @@
+package com.redhammerstudios.quickevent;
+
+import java.util.Date;
+
+/**
+ * Created by bryant on 10/21/13.
+ */
+public class Friend {
+    int id;
+    String firstName;
+    String lastName;
+    Date dateAdded;
+    String phoneNumber;
+    String userName;
+    int countOfEvents;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
