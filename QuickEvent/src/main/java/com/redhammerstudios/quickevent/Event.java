f3d19dafe180fc36135af1f1a2a604fce775bb3c
vrowold <vrowold@gmail.com>
2013-10-22 15:47:24 -0400

    Event class created

--- QuickEvent/src/main/java/com/redhammerstudios/quickevent/Event.java
+++ QuickEvent/src/main/java/com/redhammerstudios/quickevent/Event.java
@@ -1,7 +1,45 @@
 package com.redhammerstudios.quickevent;
 
+import java.util.Date;
+
 /**
  * Created by Vince on 10/21/13.
  */
 public class Event {
+
+    String eventId;
+    Date eventDate;
+    Date dateAdded;
+    String creatorUserName;
+    String creatorId;
+
+
+    public String getEventId() {return eventId;}
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+
+    }
+
 }
